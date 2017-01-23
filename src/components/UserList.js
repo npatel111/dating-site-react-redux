50ddb45c7a1563b6423ae45a3f1e966415c7d543
Niti Nrw <niti.patel@flatironschool.com>
2017-01-23 10:58:43 -0500

    Add rails back end structure

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router';
 
-const UserList = (props) => {
+class UserList extends Component {
+  constructor(props) {
+    super(props)
+    this.handleEdit = this.handleEdit.bind(this)
+    this.state = {editFormVisible: false, user: null, userInfo: {username: "", age: "", gender: "", description: ""}}
+  }
   // debugger
-  return (
-    <ul className="list-group col-lg-6 col-lg-offset-3">
-      {props.users.map((user, i) => {
-        return <li key={i} className="list-group-item"><Link to={`/users/${user.id}`} >Name: {user.username}, Age: {user.age}, Gender: {user.gender}, Description: {user.description}</Link></li>
-      })}
-      <br />
-    </ul>
-  )
+  handleEdit(event) {
+    debugger
+    event.preventDefault()
+    this.setState({editFormVisible: !this.state.editFormVisible})
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-lg-6 col-lg-offset-3">
+        {this.props.users.map((user, i) => {
+          return (
+            <div key={i}>
+              <li key={i} className="list-group-item"><Link to={`/users/${user.id}`} >Name: {user.username}, Age: {user.age}, Gender: {user.gender}, Description: {user.description}</Link>
+                <button onClick={this.handleEdit} type="submit">Edit user</button>
+              </li>
+            {this.state.editFormVisible ?
+                <form onSubmit={this.handleClick} >
+                  Edit this user: <br />
+                  <label>Name:</label>
+                  <input type="text" value={user.username}  /><br />
+                  <label>Age: </label>
+                  <input type="text" value={user.age} /><br />
+                  <label>Gender: </label>
+                  <input type="text" value={user.gender} /><br />
+                  <label>Description: </label>
+                  <input type="text" value={user.description}/><br />
+                  <input type="submit" />
+                </form>
+               : null}
+
+
+            </div>
+          )
+        })}
+        <br />
+      </ul>
+    )
+  }
+
 }
 
 export default UserList;
